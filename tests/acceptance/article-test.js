348a367ae955c53e0ef0b73b4603f377265de40d
lizlux <liz@lizlux.net>
2015-07-23 11:32:54 -0700

    adding article route and acceptance test

---
+++ tests/acceptance/article-test.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'mercury-cli/tests/helpers/start-app';
+
+var application;
+
+module('Acceptance | article', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('visiting /article', function(assert) {
+  visit('/article');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/article');
+  });
+});
