8e7c3522e9ac2f1f2f10f875cb1690761a7298ff
Andrew Gleeson <andrewgleeson@Andrews-MacBook-Pro.local>
2015-07-23 13:38:58 -0700

    added svgHelper and fixed scss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,9 +3,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    sassOptions: {
-        includePaths: ['app/styles/main']
-    }
+      
   });
 
   // Use `app.import` to add additional libraries to the generated
