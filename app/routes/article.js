bd885be4ed420bd15bd4d1ecda03378cc28cc30c
lizlux <liz@lizlux.net>
2015-07-23 14:09:05 -0700

    article route

--- app/routes/article.js
+++ app/routes/article.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model(params) {
+		let articleTitle = params.articleTitle;
+		let url = `api/v1/article/${articleTitle}`;
+		debugger;
+		return Ember.$.getJSON({
+			url: 'http://www.google.com'
+		});
+	},
+
+	afterModel() {
+		//debugger;
+	}
 });
