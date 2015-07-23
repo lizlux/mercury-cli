0015004a34004e01fbe2e488f20cb1904a1a1e7c
lizlux <liz@lizlux.net>
2015-07-23 11:46:41 -0700

    adding some components

---
+++ tests/integration/components/article-comments-test.js
@@ -0,0 +1,27 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+
+moduleForComponent('article-comments', 'Integration | Component | article comments', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{article-comments}}`);
+
+  assert.equal(this.$().text(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#article-comments}}
+      template block text
+    {{/article-comments}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
